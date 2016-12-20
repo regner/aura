694a28077320a5f8ceea4f57041682c850d353e0
Regner Blok-Andersen <regner@bench.co>
2016-12-20 14:23:01 -0800

    stuff

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let year = require('./lib/year').year;
 
-exports.root = function root(req, res) {
+exports.root = function root (req, res) {
   console.log('Request headers: ' + JSON.stringify(req.headers));
   console.log('Request body: ' + JSON.stringify(req.body));
 
   var action = req.body.result.action;
 
-  if (action == 'aura.year') {
+  if (action === 'aura.year') {
     year(req, res);
   } else {
     res.status(200);
