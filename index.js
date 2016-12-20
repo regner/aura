cdc210017e168709223c1c9e685c01fb81c4fbf2
Regner Blok-Andersen <regner@bench.co>
2016-12-20 13:12:42 -0800

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import year from './lib/year'
+
+export function root(req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  var action = req.body.result.action;
+
+  if (action == 'aura.year') {
+    year(req, res);
+  } else {
+    res.status(200);
+  }
+};
