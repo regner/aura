f97e7fd75b37491d6105141cc380ef607467c28c
Regner Blok-Andersen <regner@bench.co>
2016-12-20 17:23:06 -0800

    stuff

--- actions/year.js
+++ actions/year.js
@@ -1,15 +1,15 @@
-let Assistant = require('actions-on-google').ApiAiAssistant;
+// let Assistant = require('actions-on-google').ApiAiAssistant;
 
-exports.year = function year (req, res) {
-  const assistant = new Assistant({request: req, response: res});
-  const baseYearSubtraction = 1898;
+// exports.year = function year (req, res) {
+//   const assistant = new Assistant({request: req, response: res});
+//   const baseYearSubtraction = 1898;
 
-  var currentYear = new Date().getFullYear();
+//   var currentYear = new Date().getFullYear();
 
-  function responseHandler (assistant) {
-    assistant.tell('This was a year action... ' + (currentYear - baseYearSubtraction));
-  }
+//   function responseHandler (assistant) {
+//     assistant.tell('The current year is YC' + (currentYear - baseYearSubtraction));
+//   }
 
-  assistant.handleRequest(responseHandler);
-  res.status(200);
-};
+//   assistant.handleRequest(responseHandler);
+//   res.status(200);
+// };
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-let year = require('./actions/year').year;
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let request = require('request');
+
+const ACTION_LOGIN_COUNT = 'aura.login.count';
+const ACTION_YEAR = 'aura.year';
+
+function year (assistant) {
+  const baseYearSubtraction = 1898;
+  const currentYear = new Date().getFullYear();
+
+  assistant.tell('The current year is YC' + (currentYear - baseYearSubtraction));
+}
+
+function loginCount (assistant) {
+  request('https://crest-tq.eveonline.com/', function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body);
+
+      assistant.tell('There are currently ' + data.userCount_str + ' pilots logged into New Eden.');
+    } else {
+      assistant.tell('Sorry but it appears New Eden is dead. Please ask again later.');
+    }
+  });
+}
 
 exports.root = function root (req, res) {
   console.log('Request headers: ' + JSON.stringify(req.headers));
   console.log('Request body: ' + JSON.stringify(req.body));
 
-  var action = req.body.result.action;
+  const assistant = new Assistant({request: req, response: res});
 
-  if (action === 'aura.year') {
-    year(req, res);
-  } else {
-    res.status(200);
-  }
+  let actionMap = new Map();
+
+  actionMap.set(ACTION_LOGIN_COUNT, loginCount);
+  actionMap.set(ACTION_YEAR, year);
+
+  assistant.handleRequest(actionMap);
+
+  res.status(200);
 };
