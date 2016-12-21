ba633ad8af3e1eed4ddc50b47ff91a37c752dee3
Regner Blok-Andersen <regner@bench.co>
2016-12-20 16:15:19 -0800

    stuff

--- actions/year.js
+++ actions/year.js
@@ -2,13 +2,12 @@ let Assistant = require('actions-on-google').ApiAiAssistant;
 
 exports.year = function year (req, res) {
   const assistant = new Assistant({request: req, response: res});
-  // 1898
+  const baseYearSubtraction = 1898;
 
   var currentYear = new Date().getFullYear();
-  console.log(currentYear);
 
   function responseHandler (assistant) {
-    assistant.tell('This was a year action...');
+    assistant.tell('This was a year action... ' + (currentYear - baseYearSubtraction));
   }
 
   assistant.handleRequest(responseHandler);
